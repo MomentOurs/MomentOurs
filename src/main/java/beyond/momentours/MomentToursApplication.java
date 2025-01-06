0d40af513c11b0c2b64e7b74bc0a03ddf5cfa90a
JoJeHuni <cho9759@gmail.com>
2025-01-06 23:36:08 +0900

    🎉 프로젝트 시작 : 의존성 추가

---
+++ src/main/java/beyond/momentours/MomentToursApplication.java
@@ -0,0 +1,12 @@
+package beyond.momentours;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class MomentToursApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MomentToursApplication.class, args);
+    }
+}
