afbfc30b9741bf11d81b7af584810667801f8f0f
JoJeHuni <cho9759@gmail.com>
2025-01-16 02:18:23 +0900

    feat: 커플의 일정 월별 조회 쿼리, mapper 메서드 추가

--- src/main/java/beyond/momentours/plan/query/repository/PlanMapper.java
+++ src/main/java/beyond/momentours/plan/query/repository/PlanMapper.java
@@ -1,6 +1,10 @@
 package beyond.momentours.plan.query.repository;
 
+import beyond.momentours.plan.command.domain.aggregate.entity.Plan;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Mapper
 public interface PlanMapper {
@@ -8,4 +12,6 @@ public interface PlanMapper {
     Long findByCoupleId(Long memberId);
 
     Long findByCourseId(Long courseId);
+
+    List<Plan> findByCoupleIdAndDateRange(Long coupleId, LocalDateTime planStartDate, LocalDateTime planEndDate);
 }
