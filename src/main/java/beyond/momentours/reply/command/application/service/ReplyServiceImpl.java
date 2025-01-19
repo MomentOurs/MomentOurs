072fda175613d363dec2ab9c7614298335623e6f
JoJeHuni <cho9759@gmail.com>
2025-01-19 15:10:33 +0900

    feat: 대댓글 등록 시 댓글 검증 메서드 추가

--- src/main/java/beyond/momentours/reply/command/application/service/ReplyServiceImpl.java
+++ src/main/java/beyond/momentours/reply/command/application/service/ReplyServiceImpl.java
@@ -1,5 +1,9 @@
 package beyond.momentours.reply.command.application.service;
 
+import beyond.momentours.comment.command.domain.aggregate.entity.Comment;
+import beyond.momentours.comment.query.repository.CommentMapper;
+import beyond.momentours.common.exception.CommonException;
+import beyond.momentours.common.exception.ErrorCode;
 import beyond.momentours.reply.command.application.dto.ReplyDTO;
 import beyond.momentours.reply.command.application.mapper.ReplyConvertor;
 import beyond.momentours.reply.command.domain.aggregate.entity.Reply;
@@ -16,12 +20,15 @@ public class ReplyServiceImpl implements ReplyService {
 
     private final ReplyRepository replyRepository;
     private final ReplyConvertor replyConvertor;
+    private final CommentMapper commentDAO;
     private final ReplyMapper replyDAO;
 
     @Override
     public ReplyDTO createReply(ReplyDTO replyDTO) {
+        validateCommentId(replyDTO.getCommentId());
+
 //        Long memberId = getLoggedInMemberId(); // 로그인한 사용자의 ID 가져오기
-        Long memberId = 0L;
+        Long memberId = 0L; // (임시로 0L 사용)
         replyDTO.setMemberId(memberId);
 
         Reply reply = replyConvertor.fromDTOToEntity(replyDTO);
@@ -31,5 +38,12 @@ public class ReplyServiceImpl implements ReplyService {
         Reply savedReply = replyRepository.save(reply);
 
         return replyConvertor.fromEntityToDTO(savedReply);
+    }
+
+    private void validateCommentId(Long commentId) {
+        Comment comment = commentDAO.findCommentById(commentId);
+        if (comment == null || !comment.getCommentStatus()) {
+            throw new CommonException(ErrorCode.NOT_FOUND_COMMENT);
+        }
     }
 }
