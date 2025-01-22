3fdd410d1f26c0024376dc9850d5bfc68ae94978
JoJeHuni <cho9759@gmail.com>
2025-01-22 15:36:26 +0900

    feat: 데이트코스 등록 서비스 코드 추가

--- src/main/java/beyond/momentours/date_course/command/application/service/DateCourseServiceImpl.java
+++ src/main/java/beyond/momentours/date_course/command/application/service/DateCourseServiceImpl.java
@@ -1,8 +1,12 @@
 package beyond.momentours.date_course.command.application.service;
 
+import beyond.momentours.date_course.command.application.dto.DateCourseDTO;
 import beyond.momentours.date_course.command.application.mapper.DateCourseConverter;
+import beyond.momentours.date_course.command.domain.aggregate.entity.DateCourse;
 import beyond.momentours.date_course.command.domain.repository.DateCourseRepository;
 import beyond.momentours.date_course.query.repository.DateCourseMapper;
+import beyond.momentours.member.command.application.dto.CustomUserDetails;
+import beyond.momentours.member.query.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -15,5 +19,18 @@ public class DateCourseServiceImpl implements DateCourseService {
     private final DateCourseRepository dateCourseRepository;
     private final DateCourseConverter dateCourseConverter;
     private final DateCourseMapper dateCourseDAO;
+    private final MemberService memberService;
 
+    @Override
+    public DateCourseDTO createDateCourse(DateCourseDTO dateCourseDTO, CustomUserDetails user) {
+        Long memberId = memberService.findByMemberId(user.getUsername());
+        dateCourseDTO.setMemberId(memberId);
+
+        DateCourse dateCourse = dateCourseConverter.fromDTOToEntity(dateCourseDTO);
+        log.info("저장할 데이트 코스 : {}", dateCourse);
+
+        DateCourse savedCourse = dateCourseRepository.save(dateCourse);
+
+        return dateCourseConverter.fromEntityToDTO(savedCourse);
+    }
 }
