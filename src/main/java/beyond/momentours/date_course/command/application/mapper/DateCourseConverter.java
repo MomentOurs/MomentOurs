80601a6b0aec2fb004ea40ac22159e21480331e6
JoJeHuni <cho9759@gmail.com>
2025-01-22 15:37:08 +0900

    feat: 데이트코스 등록 api 추가 및 관련 VO, Mapper 추가

--- src/main/java/beyond/momentours/date_course/command/application/mapper/DateCourseConverter.java
+++ src/main/java/beyond/momentours/date_course/command/application/mapper/DateCourseConverter.java
@@ -1,7 +1,68 @@
 package beyond.momentours.date_course.command.application.mapper;
 
+import beyond.momentours.date_course.command.application.dto.DateCourseDTO;
+import beyond.momentours.date_course.command.domain.aggregate.entity.DateCourse;
+import beyond.momentours.date_course.command.domain.vo.request.RequestCreateDateCourseVO;
+import beyond.momentours.date_course.command.domain.vo.response.ResponseCreateDateCourseVO;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DateCourseConverter {
+    public DateCourseDTO fromCreateVOToDTO(RequestCreateDateCourseVO request) {
+        return DateCourseDTO.builder()
+                .courseTitle(request.getCourseTitle())
+                .courseType(request.getCourseType())
+                .courseMemo(request.getCourseMemo())
+                .courseStartDate(request.getCourseStartDate())
+                .courseEndDate(request.getCourseEndDate())
+                .build();
+    }
+
+    public ResponseCreateDateCourseVO fromDTOToCreateVO(DateCourseDTO saveDateCourseDTO) {
+        return ResponseCreateDateCourseVO.builder()
+                .courseId(saveDateCourseDTO.getCourseId())
+                .courseTitle(saveDateCourseDTO.getCourseTitle())
+                .courseType(saveDateCourseDTO.getCourseType())
+                .courseMemo(saveDateCourseDTO.getCourseMemo())
+                .courseDisclosure(saveDateCourseDTO.getCourseDisclosure())
+                .courseLike(saveDateCourseDTO.getCourseLike())
+                .courseView(saveDateCourseDTO.getCourseView())
+                .courseStatus(saveDateCourseDTO.getCourseStatus())
+                .courseStartDate(saveDateCourseDTO.getCourseStartDate())
+                .courseEndDate(saveDateCourseDTO.getCourseEndDate())
+                .createdAt(saveDateCourseDTO.getCreatedAt())
+                .updatedAt(saveDateCourseDTO.getUpdatedAt())
+                .memberId(saveDateCourseDTO.getMemberId())
+                .build();
+    }
+
+    public DateCourse fromDTOToEntity(DateCourseDTO dateCourseDTO) {
+        return DateCourse.builder()
+                .courseTitle(dateCourseDTO.getCourseTitle())
+                .courseType(dateCourseDTO.getCourseType())
+                .courseMemo(dateCourseDTO.getCourseMemo())
+                .courseDisclosure(dateCourseDTO.getCourseDisclosure())
+                .courseStartDate(dateCourseDTO.getCourseStartDate())
+                .courseEndDate(dateCourseDTO.getCourseEndDate())
+                .memberId(dateCourseDTO.getMemberId())
+                .build();
+    }
+
+    public DateCourseDTO fromEntityToDTO(DateCourse savedCourse) {
+        return DateCourseDTO.builder()
+                .courseId(savedCourse.getCourseId())
+                .courseTitle(savedCourse.getCourseTitle())
+                .courseType(savedCourse.getCourseType())
+                .courseMemo(savedCourse.getCourseMemo())
+                .courseDisclosure(savedCourse.getCourseDisclosure())
+                .courseLike(savedCourse.getCourseLike())
+                .courseView(savedCourse.getCourseView())
+                .courseStatus(savedCourse.getCourseStatus())
+                .courseStartDate(savedCourse.getCourseStartDate())
+                .courseEndDate(savedCourse.getCourseEndDate())
+                .createdAt(savedCourse.getCreatedAt())
+                .updatedAt(savedCourse.getUpdatedAt())
+                .memberId(savedCourse.getMemberId())
+                .build();
+    }
 }
---
+++ src/main/java/beyond/momentours/date_course/command/domain/vo/request/RequestCreateDateCourseVO.java
@@ -0,0 +1,20 @@
+package beyond.momentours.date_course.command.domain.vo.request;
+
+import beyond.momentours.date_course.command.domain.aggregate.CourseType;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class RequestCreateDateCourseVO {
+    private String courseTitle;
+    private CourseType courseType;
+    private String courseMemo;
+    private Boolean courseDisclosure;
+    private LocalDateTime courseStartDate;
+    private LocalDateTime courseEndDate;
+}
