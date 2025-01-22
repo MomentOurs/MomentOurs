13e3b3c3a1d89d04e60bb241b103705aabe2a78d
Vorschlag-bit <bustout@naver.com>
2025-01-22 12:52:04 +0900

    feat: 존재하지 않는 커플로 글 생성 시 예외 발생 테스트 및 요청한 커플 ID가 실제 커플 ID와 불일치 할 경우 테스트 코드 추가

--- src/test/java/beyond/momentours/couplelog/command/application/service/CommandCoupleLogServiceImplTests.java
+++ src/test/java/beyond/momentours/couplelog/command/application/service/CommandCoupleLogServiceImplTests.java
@@ -1,5 +1,7 @@
 package beyond.momentours.couplelog.command.application.service;
 
+import beyond.momentours.common.exception.CommonException;
+import beyond.momentours.common.exception.ErrorCode;
 import beyond.momentours.couple.command.application.dto.CoupleListDTO;
 import beyond.momentours.couple.query.service.QueryCoupleService;
 import beyond.momentours.couplelog.command.domain.aggregate.entity.CoupleLog;
@@ -17,8 +19,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @Transactional
@@ -31,7 +32,7 @@ class CommandCoupleLogServiceImplTests {
     private QueryCoupleService coupleService;
 
     @InjectMocks
-    private CommandCoupleLogService service;
+    private CommandCoupleLogServiceImpl service;
 
     private CustomUserDetails user;
     private CoupleListDTO couple;
@@ -48,7 +49,7 @@ class CommandCoupleLogServiceImplTests {
     }
 
     @Test
-    @DisplayName("새로운 커플로그 작성 테스트")
+    @DisplayName("새로운 커플로그 작성 성공 테스트")
     void createNewCoupleLog() {
         // given
         CoupleLogRequestVO requestVO = CoupleLogRequestVO.builder()
@@ -75,5 +76,43 @@ class CommandCoupleLogServiceImplTests {
                 () -> assertEquals(requestVO.getCoupleId(), savedLog.getCoupleId()),
                 () -> assertEquals(user.getMember().getMemberId(), savedLog.getMemberId())
         );
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 커플로 글 생성 시 예외 발생 테스트")
+    void createNewCoupleLogWithInvalidMemberId() {
+        // given
+        CoupleLogRequestVO requestVO = CoupleLogRequestVO.builder()
+                .coupleId(1L)
+                .textContent("테스트 커플로그 텍스트")
+                .build();
+        when(coupleService.getCoupleByMemberId(user.getMember().getMemberId())).thenReturn(null);
+
+        // when & then
+        assertThrows(CommonException.class, () ->
+                service.createNewCoupleLog(requestVO, user));
+
+        // repository 저장 안 됌 확인
+        verify(coupleLogRepository, never()).save(any(CoupleLog.class));
+    }
+
+    @Test
+    @DisplayName("요청한 커플ID가 실제 커플ID와 불일치 할 경우 테스트")
+    void createNewCoupleLogWithInvalidCoupleId() {
+        // given
+        CoupleLogRequestVO requestVO = CoupleLogRequestVO.builder()
+                .coupleId(2L)
+                .textContent("테스트 커플로그 텍스트")
+                .build();
+
+        // coupleDTO의 커플 번호는 1L
+        when(coupleService.getCoupleByMemberId(user.getMember().getMemberId())).thenReturn(couple);
+
+        // when & then
+        CommonException exception = assertThrows(CommonException.class, () ->
+                service.createNewCoupleLog(requestVO, user));
+
+        assertEquals(exception.getErrorCode(), ErrorCode.FORBIDDEN_ROLE);
+        verify(coupleLogRepository, never()).save(any(CoupleLog.class));
     }
 }
