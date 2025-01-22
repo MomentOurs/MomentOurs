5b1d7ef0d582805134540a2ccedc5732835679e0
JoJeHuni <cho9759@gmail.com>
2025-01-22 15:35:42 +0900

    feat: 데이트코스 열거형 데이트, 여행 타입으로 설정

--- src/main/java/beyond/momentours/date_course/command/domain/aggregate/CourseType.java
+++ src/main/java/beyond/momentours/date_course/command/domain/aggregate/CourseType.java
@@ -1,5 +1,17 @@
 package beyond.momentours.date_course.command.domain.aggregate;
 
-public enum CourseType {
+import com.fasterxml.jackson.annotation.JsonValue;
 
+public enum CourseType {
+    DATE("DATE"),
+    TRIP("TRIP");
+
+    private final String courseType;
+
+    CourseType(String courseType) { this.courseType = courseType; }
+
+    @JsonValue
+    public String getType() {
+        return courseType;
+    }
 }
