96c1bbfeb4f0c36d129bdcebe4564ffc909153a3
JoJeHuni <cho9759@gmail.com>
2025-01-28 00:16:15 +0900

    feat: 데이트 코스 본인이 등록한 데이트 코스들 조회하는 쿼리문 추가

--- src/main/java/beyond/momentours/date_course/query/repository/DateCourseMapper.java
+++ src/main/java/beyond/momentours/date_course/query/repository/DateCourseMapper.java
@@ -12,4 +12,6 @@ public interface DateCourseMapper {
     Optional<DateCourse> findActiveById(@Param("courseId") Long courseId);
     List<DateCourse> findCoursesWithSorting(@Param("sortBy") String sortBy);
     void incrementViewCountInDB(@Param("courseId") Long courseId, @Param("viewCount") Long viewCount);
+
+    List<DateCourse> findCoursesByMemberId(@Param("memberId") Long memberId);
 }
