75d9faf99a402a0f7f9eebda4ee797238bad84cf
JoJeHuni <cho9759@gmail.com>
2025-01-27 23:44:24 +0900

    feat: 데이트 코스 전체 조회, 상세 조회를 위한 쿼리문 추가

--- src/main/java/beyond/momentours/date_course/query/repository/DateCourseMapper.java
+++ src/main/java/beyond/momentours/date_course/query/repository/DateCourseMapper.java
@@ -4,9 +4,12 @@ import beyond.momentours.date_course.command.domain.aggregate.entity.DateCourse;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 @Mapper
 public interface DateCourseMapper {
     Optional<DateCourse> findActiveById(@Param("courseId") Long courseId);
+    List<DateCourse> findCoursesWithSorting(@Param("sortBy") String sortBy);
+    void incrementViewCountInDB(@Param("courseId") Long courseId, @Param("viewCount") Long viewCount);
 }
