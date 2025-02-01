bea7ecb4c52e70763834839e574b431b3290237a
JoJeHuni <cho9759@gmail.com>
2025-02-01 22:00:45 +0900

    feat: 데이트코스 즐겨찾기 DTO, VO 매퍼 추가

---
+++ src/main/java/beyond/momentours/course_scrap/command/application/mapper/CourseScrapConverter.java
@@ -0,0 +1,45 @@
+package beyond.momentours.course_scrap.command.application.mapper;
+
+import beyond.momentours.course_scrap.command.application.dto.CourseScrapDTO;
+import beyond.momentours.course_scrap.command.domain.aggregate.entity.CourseScrap;
+import beyond.momentours.course_scrap.command.domain.vo.request.RequestCreateCourseScrapVO;
+import beyond.momentours.course_scrap.command.domain.vo.response.ResponseCreateCourseScrapVO;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CourseScrapConverter {
+
+    public CourseScrapDTO fromCreateVOToDTO(RequestCreateCourseScrapVO vo) {
+        return CourseScrapDTO.builder()
+                .courseScrapFolderId(vo.getCourseScrapFolderId())
+                .courseId(vo.getCourseId())
+                .build();
+    }
+
+    public ResponseCreateCourseScrapVO fromDTOToCreateVO(CourseScrapDTO dto) {
+        return ResponseCreateCourseScrapVO.builder()
+                .courseScrapId(dto.getCourseScrapId())
+                .courseScrapFolderId(dto.getCourseScrapFolderId())
+                .courseId(dto.getCourseId())
+                .createdAt(dto.getCreatedAt())
+                .build();
+    }
+
+    public CourseScrapDTO fromEntityToDTO(CourseScrap entity) {
+        return CourseScrapDTO.builder()
+                .courseScrapId(entity.getCourseScrapId())
+                .courseScrapFolderId(entity.getCourseScrapFolderId())
+                .courseId(entity.getCourseId())
+                .createdAt(entity.getCreatedAt())
+                .build();
+    }
+
+    public CourseScrap fromDTOToEntity(CourseScrapDTO scrapDTO) {
+        return CourseScrap.builder()
+                .courseScrapId(scrapDTO.getCourseId())
+                .courseScrapFolderId(scrapDTO.getCourseScrapFolderId())
+                .courseId(scrapDTO.getCourseId())
+                .createdAt(scrapDTO.getCreatedAt())
+                .build();
+    }
+}
