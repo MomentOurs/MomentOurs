266fc41fbe1bf02d6db6bd7d31c282ae403bb2c6
JoJeHuni <cho9759@gmail.com>
2025-02-05 21:15:18 +0900

    feat: Report 등록 VO 추가

---
+++ src/main/java/beyond/momentours/report/command/domain/vo/response/ResponseCreateReportVO.java
@@ -0,0 +1,25 @@
+package beyond.momentours.report.command.domain.vo.response;
+
+import beyond.momentours.report.command.domain.aggregate.ProcessStatus;
+import beyond.momentours.report.command.domain.aggregate.ReportReason;
+import beyond.momentours.report.command.domain.aggregate.ReportType;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class ResponseCreateReportVO {
+    private Long reportId;
+    private ReportType reportType;
+    private ReportReason reportReason;
+    private ProcessStatus processStatus;
+    private LocalDateTime createdAt;
+    private Long reportedUserId;
+    private Long reportUserId;
+    private Long momentId;
+    private Long courseId;
+}
