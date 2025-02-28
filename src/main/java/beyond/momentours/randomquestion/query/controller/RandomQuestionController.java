e06a54e13752011c2c060fd5f9e70dd104658b7f
yuhyejin <hj_eu@naver.com>
2025-03-01 01:10:19 +0900

    fix: 전체 목록 페이징 수정중...

--- src/main/java/beyond/momentours/randomquestion/query/controller/RandomQuestionController.java
+++ src/main/java/beyond/momentours/randomquestion/query/controller/RandomQuestionController.java
@@ -16,10 +16,6 @@ import org.springframework.web.bind.annotation.*;
 public class RandomQuestionController {
 
     private final RandomQuestionService randomQuestionService;
-    // createPlan
-    //updatePlan
-    //deletePlan
-    //getPlanBy~~
 
     // answer_status는 초기값이 0이고, 둘다 답변하면 1이고, 한명만 답변했다면 그 사람의 회원번호
     @GetMapping("/")
