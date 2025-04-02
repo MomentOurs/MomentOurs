e8fc5980d60d32b8eca715c0305c6333b51d8fb2
JoJeHuni <cho9759@gmail.com>
2025-04-02 21:37:50 +0900

    fix: 폴더 사진 추가

--- src/main/java/beyond/momentours/course_scrap_folder/command/domain/aggregate/entity/CourseScrapFolder.java
+++ src/main/java/beyond/momentours/course_scrap_folder/command/domain/aggregate/entity/CourseScrapFolder.java
@@ -21,6 +21,9 @@ public class CourseScrapFolder {
     @Column(name = "folder_name", nullable = false)
     private String folderName;
 
+    @Column(name = "folder_image")
+    private String folderImage;
+
     @Column(name = "member_id", nullable = false)
     private Long memberId;
 
