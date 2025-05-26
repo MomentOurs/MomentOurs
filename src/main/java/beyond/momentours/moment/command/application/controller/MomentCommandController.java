8a53eb86484defb7a059bc267e971a88342a904f
JoJeHuni <cho9759@gmail.com>
2025-05-26 17:38:14 +0900

    refactor: momentController -> MomentCommandController 로 변경 후 추억 등록 기능 수정

---
+++ src/main/java/beyond/momentours/moment/command/application/controller/MomentCommandController.java
@@ -0,0 +1,39 @@
+package beyond.momentours.moment.command.application.controller;
+
+import beyond.momentours.common.ResponseDTO;
+import beyond.momentours.member.command.application.dto.CustomUserDetails;
+import beyond.momentours.moment.command.application.dto.MomentDTO;
+import beyond.momentours.moment.command.application.service.MomentService;
+import beyond.momentours.moment.command.domain.aggregate.vo.request.RequestCreateMomentVO;
+import beyond.momentours.moment.common.converter.MomentConverter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("api/moment")
+@RequiredArgsConstructor
+public class MomentCommandController {
+
+    private final MomentService momentService;
+    private final MomentConverter momentConverter;
+
+    @PostMapping
+    public ResponseDTO<?> createMoment(@RequestBody RequestCreateMomentVO createMomentVO, @AuthenticationPrincipal CustomUserDetails user) {
+        log.info("등록 요청 데이터 : {}", createMomentVO);
+        Long memberId = user.getMember().getMemberId();
+        MomentDTO momentDTO = momentConverter.fromCreateVOToDTO(createMomentVO);
+        MomentDTO responseMomentDTO = momentService.createMoment(momentDTO, memberId);
+        return ResponseDTO.ok(responseMomentDTO);
+    }
+
+    @PatchMapping
+    public ResponseDTO<?> updateMoment(@RequestBody MomentDTO momentDTO, @AuthenticationPrincipal CustomUserDetails user) {
+        log.info("수정 요청 데이터 : {}", momentDTO);
+        MomentDTO responseMomentDTO = momentService.updateMoment(momentDTO, user);
+        return ResponseDTO.ok(responseMomentDTO);
+    }
+
+}
