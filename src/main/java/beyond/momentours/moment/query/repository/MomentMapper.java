772040310e8acc921fc0fc4f7d07a5f306898349
JoJeHuni <cho9759@gmail.com>
2025-05-27 10:38:13 +0900

    feat: 추억 상세 관련 쿼리문, api, vo 및 추억 커서 페이지네이션 방식을 위한 vo 추가

--- src/main/java/beyond/momentours/moment/query/repository/MomentMapper.java
+++ src/main/java/beyond/momentours/moment/query/repository/MomentMapper.java
@@ -1,7 +1,16 @@
 package beyond.momentours.moment.query.repository;
 
+import beyond.momentours.moment.query.vo.ResponseMomentDetailVO;
+import beyond.momentours.moment.query.vo.ResponseMomentListItemVO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface MomentMapper {
+    List<ResponseMomentListItemVO> findMomentsByLocationIdWithCursor(@Param("locationId") Long locationId, @Param("cursor") Long cursor, @Param("size") int size);
+
+    ResponseMomentDetailVO findMomentDetailById(@Param("momentId") Long momentId);
+
 }
